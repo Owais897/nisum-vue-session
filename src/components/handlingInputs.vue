2b4109aa6378e8ded31e5130c88d1991c239e217
Muhammad Owais <m.owais2118@gmail.com>
2021-10-28 04:06:40 +0500

    first 3 examples

---
+++ src/components/handlingInputs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ text }}
+    <input v-model="text" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "handlingInputs",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style></style>
